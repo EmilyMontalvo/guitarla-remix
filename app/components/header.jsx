cfb88b2b475d61cefc9e10115c2532d584e6b9ba
EmilyMontalvo25 <emily.montalvo@epn.edu.ec>
2024-02-13 12:12:34 -0500

    add header styles

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import { Link } from '@remix-run/react'
+import { 
+  Link, 
+  useLocation //Brinda informacion de la ubicacion actual
+} from '@remix-run/react'
+import logo from '../../public/img/logo.svg'
 
 const Header = () => {
+
+  const location = useLocation() //Brinda inofrmacion de la ubicacion actual
+ 
+
   return (
     <header className='header'>
       <div className='contenedor barra'>
-        <div className="logo"></div>
+        <Link className="logo" to='/'>
+          <img className='logo' src={logo} alt="Imagen logo" />
+        </Link>
         <nav className="navegacion">
-          <Link to="/" prefetch='render'> Inicio </Link>
-          <Link to="/nosotros" prefetch='render'> Nosotros </Link>
-          <Link to="/tienda" prefetch='render'> Tienda </Link>
-          <Link to="/blog" prefetch='render'> Blog </Link>
+          <Link to="/" className={location.pathname === '/' ? 'active': ''} > Inicio </Link>
+          <Link to="/nosotros" className={location.pathname === '/nosotros' ? 'active': ''} > Nosotros </Link>
+          <Link to="/tienda" className={location.pathname === '/tienda' ? 'active': ''} > Tienda </Link>
+          <Link to="/blog" className={location.pathname === '/blog' ? 'active': ''} > Blog </Link>
         </nav>
       </div>
     </header>
